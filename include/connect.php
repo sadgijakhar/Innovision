a652fddb266ecb37dffc83399aa3aab4b566887b
Sadgi Jakhar <sadgijakhar@Sadgis-MacBook-Air.local>
2023-02-22 17:24:34 +0530

    update

---
+++ include/connect.php
@@ -0,0 +1,10 @@
+<?php
+
+$con=new mysqli('localhost','root','','Inovision');
+
+if(!$con){
+    // 
+    die(mysqli_error($con));
+}
+
+?>
---
+++ index.php
@@ -0,0 +1,160 @@
+<?php
+include('/include/connect.php');
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Event Hunt</title>
+    <link rel="stylesheet" href="index.css">
+</head>
+
+<body>
+    <!------------------------------------- HEADER ---------------------------------------------------->
+    <section class="header">
+        <img class="header_img" src="images/Black Minimalist Initial Font BE Logo.png" alt="">
+        <div class="links">
+            <div>Events</div>
+            <div>locations</div>
+            <div>Maps</div>
+        </div>
+        <div class="profile"></div>
+    </section>
+    <!------------------------------------- MAP ---------------------------------------------------->
+    <section class="content_body">
+        <section class="main_map">
+            <div class="map"></div>
+        </section>
+        <!------------------------------------- CURRENT EVENTS ---------------------------------------------------->
+        <section class="c_events">
+            <h1 class="current_events">Current Events</h1>
+            <div class="event_list">
+                <hr class="main_hr">
+                <a class="event_s_i" href="#">
+                    <h2 class="e_n">Name of the Fest</h2>
+                    <div class="e_d">Date: dd month, yyyy</div>
+                    <div class="e_l">Location: Home, Street, city, country</div>
+                    <div class="e_a">About: Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore dolorem
+                        odio accusamus laboriosam quae. Expedita est enim eum reprehenderit non. Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit, nemo minus quisquam maxime iure voluptates totam eveniet ad quas cupi...</div>
+                </a>
+                <hr class="hr">
+                <a class="event_s_i" href="#">
+                    <h2 class="e_n">Name of the Fest</h2>
+                    <div class="e_d">Date: dd month, yyyy</div>
+                    <div class="e_l">Location: Home, Street, city, country</div>
+                    <div class="e_a">About: Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore dolorem
+                        odio accusamus laboriosam quae. Expedita est enim eum reprehenderit non. Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit, nemo minus quisquam maxime iure voluptates totam eveniet ad quas cupi...</div>
+                </a>
+                <hr class="hr">
+                <a class="event_s_i" href="#">
+                    <h2 class="e_n">Name of the Fest</h2>
+                    <div class="e_d">Date: dd month, yyyy</div>
+                    <div class="e_l">Location: Home, Street, city, country</div>
+                    <div class="e_a">About: Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore dolorem
+                        odio accusamus laboriosam quae. Expedita est enim eum reprehenderit non. Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit, nemo minus quisquam maxime iure voluptates totam eveniet ad quas cupi...</div>
+                </a>
+                <hr class="hr">
+                <a class="event_s_i" href="#">
+                    <h2 class="e_n">Name of the Fest</h2>
+                    <div class="e_d">Date: dd month, yyyy</div>
+                    <div class="e_l">Location: Home, Street, city, country</div>
+                    <div class="e_a">About: Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore dolorem
+                        odio accusamus laboriosam quae. Expedita est enim eum reprehenderit non. Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit, nemo minus quisquam maxime iure voluptates totam eveniet ad quas cupi...</div>
+                </a>
+                <hr class="hr">
+                <div class="sm_btn_div"><button class="sm sm_index">Show More</button></div>
+                
+
+            </div>
+        </section>
+        <!------------------------------------- UP COMING EVENTS---------------------------------------------------->
+        <section class="c_events">
+            <h1 class="current_events">Up Coming Events</h1>
+            <div class="event_list">
+                <hr class="main_hr">
+                <a class="event_s_i" href="#">
+                    <h2 class="e_n">Name of the Fest</h2>
+                    <div class="e_d">Date: dd month, yyyy</div>
+                    <div class="e_l">Location: Home, Street, city, country</div>
+                    <div class="e_a">About: Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore dolorem
+                        odio accusamus laboriosam quae. Expedita est enim eum reprehenderit non. Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit, nemo minus quisquam maxime iure voluptates totam eveniet ad quas cupi...</div>
+                </a>
+                <hr class="hr">
+                <a class="event_s_i" href="#">
+                    <h2 class="e_n">Name of the Fest</h2>
+                    <div class="e_d">Date: dd month, yyyy</div>
+                    <div class="e_l">Location: Home, Street, city, country</div>
+                    <div class="e_a">About: Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore dolorem
+                        odio accusamus laboriosam quae. Expedita est enim eum reprehenderit non. Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit, nemo minus quisquam maxime iure voluptates totam eveniet ad quas cupi...</div>
+                </a>
+                <hr class="hr">
+                <a class="event_s_i" href="#">
+                    <h2 class="e_n">Name of the Fest</h2>
+                    <div class="e_d">Date: dd month, yyyy</div>
+                    <div class="e_l">Location: Home, Street, city, country</div>
+                    <div class="e_a">About: Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore dolorem
+                        odio accusamus laboriosam quae. Expedita est enim eum reprehenderit non. Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit, nemo minus quisquam maxime iure voluptates totam eveniet ad quas cupi...</div>
+                </a>
+                <hr class="hr">
+                <a class="event_s_i" href="#">
+                    <h2 class="e_n">Name of the Fest</h2>
+                    <div class="e_d">Date: dd month, yyyy</div>
+                    <div class="e_l">Location: Home, Street, city, country</div>
+                    <div class="e_a">About: Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore dolorem
+                        odio accusamus laboriosam quae. Expedita est enim eum reprehenderit non. Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit, nemo minus quisquam maxime iure voluptates totam eveniet ad quas cupi...</div>
+                </a>
+                <hr class="hr">
+                <div class="sm_btn_div"><button class="sm sm_index">Show More</button></div>
+                
+
+            </div>
+        </section>
+        <!------------------------------------- FUTURE EVENTS ---------------------------------------------------->
+        <section class="c_events">
+            <h1 class="current_events">Future Events</h1>
+            <div class="event_list">
+                <hr class="main_hr">
+                <a class="event_s_i" href="#">
+                    <h2 class="e_n">Name of the Fest</h2>
+                    <div class="e_d">Date: dd month, yyyy</div>
+                    <div class="e_l">Location: Home, Street, city, country</div>
+                    <div class="e_a">About: Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore dolorem
+                        odio accusamus laboriosam quae. Expedita est enim eum reprehenderit non. Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit, nemo minus quisquam maxime iure voluptates totam eveniet ad quas cupi...</div>
+                </a>
+                <a class="event_s_i" href="#">
+                    <h2 class="e_n">Name of the Fest</h2>
+                    <div class="e_d">Date: dd month, yyyy</div>
+                    <div class="e_l">Location: Home, Street, city, country</div>
+                    <div class="e_a">About: Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore dolorem
+                        odio accusamus laboriosam quae. Expedita est enim eum reprehenderit non. Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit, nemo minus quisquam maxime iure voluptates totam eveniet ad quas cupi...</div>
+                </a>
+                <a class="event_s_i" href="#">
+                    <h2 class="e_n">Name of the Fest</h2>
+                    <div class="e_d">Date: dd month, yyyy</div>
+                    <div class="e_l">Location: Home, Street, city, country</div>
+                    <div class="e_a">About: Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore dolorem
+                        odio accusamus laboriosam quae. Expedita est enim eum reprehenderit non. Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit, nemo minus quisquam maxime iure voluptates totam eveniet ad quas cupi...</div>
+                </a>
+                <a class="event_s_i" href="#">
+                    <h2 class="e_n">Name of the Fest</h2>
+                    <div class="e_d">Date: dd month, yyyy</div>
+                    <div class="e_l">Location: Home, Street, city, country</div>
+                    <div class="e_a">About: Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore dolorem
+                        odio accusamus laboriosam quae. Expedita est enim eum reprehenderit non. Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit, nemo minus quisquam maxime iure voluptates totam eveniet ad quas cupi...</div>
+                </a>
+                <div class="sm_btn_div"><button class="sm sm_index">Show More</button></div>
+                
+
+            </div>
+        </section>
+    </section>
+    <section class="footer">
+
+    </section>
+</body>
+
+</html>
